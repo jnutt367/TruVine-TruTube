c7be08b7d15b079099e2017319843fbd683c6527
Jason Nutt <jnutt367@gmail.com>
2026-05-05 19:56:11 -0400

    Update video source in IntroVideo component

--- src/components/IntroVideo.jsx
+++ src/components/IntroVideo.jsx
@@ -55,7 +55,7 @@ export default function TruVineHome() {
 
         <div style={styles.playerWrapper}>
           <iframe
-            src="https://www.youtube.com/embed/B0JO8BNRyfE?rel=0"
+            src="https://www.youtube.com/embed/rh5YzcTMRqg?rel=0"
             title="Featured Video"
             frameBorder="0"
             allowFullScreen
